d7dd98a7bd8ec65d46028d547f2b84bdd798373f
Kowsor Dip <mddip2000@gmail.com>
2026-04-10 17:49:38 +0600

    added basic registration form

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -14,7 +14,18 @@ const Register = () => {
         <label htmlFor="Last-name">Last name</label> <br />
         <input type="text" id="Last-name" name="Last-name" /> <br />
         <label htmlFor="date-of-birth">Date of birth</label>
-        <input type="date" name="date-of-birth" id="date-of-birth" />
+        <input type="date" name="date-of-birth" id="date-of-birth" /> <br />
+        <label>Gender</label> <br />
+        <label htmlFor="male">Male</label>
+        <input type="radio" id="male" name="gender" value="male" />
+        <label htmlFor="female">Female</label>
+        <input type="radio" id="female" name="gender" value="female" />
+        <label htmlFor="other">Other</label>
+        <input type="radio" id="other" name="gender" value="other" /> <br />
+        <label htmlFor="bg">Blood group</label>
+        {/* will change later */}
+        <input type="text" name="bg" id="bg" /> <br />
+        <button>Continue</button>
       </form>
     </div>
     </div>

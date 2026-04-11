717ea820187530611ac8c95a1af88dc97fd479fa
Kowsor Dip <mddip2000@gmail.com>
2026-04-11 21:58:28 +0600

    Designed 3 seperate components for Sign up page

---
+++ src/components/Signup/Personal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Personal = () => {
+  return (
+    <form>
+        <h4>PERSONAL INFORMATION</h4>
+        <label htmlFor="First-name">First name</label> <br />
+        <input type="text" id="First-name" name="First-name" /> <br />
+        <label htmlFor="Last-name">Last name</label> <br />
+        <input type="text" id="Last-name" name="Last-name" /> <br />
+        <label htmlFor="date-of-birth">Date of birth</label>
+        <input type="date" name="date-of-birth" id="date-of-birth" /> <br />
+        <label>Gender</label> <br />
+        <label htmlFor="male">Male</label>
+        <input type="radio" id="male" name="gender" value="male" />
+        <label htmlFor="female">Female</label>
+        <input type="radio" id="female" name="gender" value="female" />
+        <label htmlFor="other">Other</label>
+        <input type="radio" id="other" name="gender" value="other" /> <br />
+        <label htmlFor="bg">Blood group</label>
+        {/* will change later */}
+        <input type="text" name="bg" id="bg" /> <br />
+        <button>Continue</button>
+      </form>
+  )
+}
+
+export default Personal
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Personal from "../../components/Signup/Personal";
 
 const Register = () => {
   return (
@@ -7,26 +8,7 @@ const Register = () => {
       <h2>Create your account</h2>
       <p>Free patient account — book services and track appointments</p>
     <div className="form">
-      <form>
-        <h4>PERSONAL INFORMATION</h4>
-        <label htmlFor="First-name">First name</label> <br />
-        <input type="text" id="First-name" name="First-name" /> <br />
-        <label htmlFor="Last-name">Last name</label> <br />
-        <input type="text" id="Last-name" name="Last-name" /> <br />
-        <label htmlFor="date-of-birth">Date of birth</label>
-        <input type="date" name="date-of-birth" id="date-of-birth" /> <br />
-        <label>Gender</label> <br />
-        <label htmlFor="male">Male</label>
-        <input type="radio" id="male" name="gender" value="male" />
-        <label htmlFor="female">Female</label>
-        <input type="radio" id="female" name="gender" value="female" />
-        <label htmlFor="other">Other</label>
-        <input type="radio" id="other" name="gender" value="other" /> <br />
-        <label htmlFor="bg">Blood group</label>
-        {/* will change later */}
-        <input type="text" name="bg" id="bg" /> <br />
-        <button>Continue</button>
-      </form>
+      <Personal />
     </div>
     </div>
   );

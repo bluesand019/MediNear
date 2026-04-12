bf62f1757854bf9ff01270874ceb3d31853bde26
Kowsor Dip <mddip2000@gmail.com>
2026-04-12 14:55:14 +0600

    Implemented DoctorCard

---
+++ src/components/layout/DoctorCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DoctorCard = ({name, gender, department, experience, rating, distance, hospital}) => {
+  return (
+    <li>
+      <h4>{name}</h4>
+      <p>{gender}</p>
+      <p>{department}</p>
+      <p>{experience} years experience</p>
+      <p>&#11088; {rating}</p>
+      <p>{distance} km away</p>
+      <p>{hospital}</p>
+      <button>View profile</button>
+      <button>Book now</button>
+    </li>
+  )
+}
+
+export default DoctorCard
--- src/pages/DoctorSearch.jsx
+++ src/pages/DoctorSearch.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import DoctorCard from '../components/layout/DoctorCard'
 
 const DoctorSearch = () => {
+  const DUMMY_DOCTORS = [{
+    name: "Dr. Sadia Islam",
+    gender: "Female",
+    department: "Gynecologist",
+    experience: 8,
+    rating: 4.9,
+    distance: 0.8,
+    hopital: "Ibn Sina Hospital, Rajshahi"
+  }]
   return (
     <div>
-      Search doctors here.
+      
     </div>
   )
 }
